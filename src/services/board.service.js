d09782d70d324620c5190f17d1eb8dbf9b1ab704
huy <okey222k@gmail.com>
2022-03-15 13:27:22 +0700

    Create API Get full Trello Board with Columns & Cards

--- src/services/board.service.js
+++ src/services/board.service.js
@@ -1,20 +1,28 @@
-import { BoardModel } from "*/models/board.model"
+import { BoardModel } from "*/models/board.model";
 
-const createNewBoard = async(data)=>{
-    try {
-        const result = await BoardModel.createNewBoard(data)
-        return result
-    } catch (error) {
-        throw new Error(error)
-    }
-}
+const createNewBoard = async (data) => {
+  try {
+    const result= await BoardModel.createNewBoard(data);
+    return result;
+  } catch (error) {
+    throw new Error(error);
+  }
+};
 
-const getFullBoard = async(boardId)=>{
-    try {
-        const result = await BoardModel.getFullBoard(boardId)
-        return result
-    } catch (error) {
-        throw new Error(error)
-    }
-}
-export const BoardServices = { createNewBoard,getFullBoard }
+const getFullBoard = async (boardId) => {
+  try {
+    const board = await BoardModel.getFullBoard(boardId);
+    // Add card to each column
+    // board.columnsArray.forEach(
+    //     (column) =>
+    //       (column.cardsArray = board.cardsArray.filter(
+    //         (card) => card.columnId.toString() === column._id.toString()
+    //       ))
+    //   );
+    // delete board.cardsArray;
+    return board;
+  } catch (error) {
+    throw new Error(error);
+  }
+};
+export const BoardServices = { createNewBoard, getFullBoard };
